37f5ae6ff3d98531a2c4ae6f4a90cb783bb9a86b
Carmen V Ramos <carmenvramos@yahoo.com>
2018-11-27 15:11:10 -0800

    Created App.vue, Pokemons.vue, Pokemon.vue and wired them together; and  pokemon list rendering successfully

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+      <img v-bind:src="pokemon.url_image"/>
+      <div class="info">
+          <h3>{{ pokemon.pokemon}}</h3>
+          <p>type-1: {{ pokemon.type-1 }}</p>
+          <p>attack: {{ pokemon.attack }}</p>
+      </div>
+  </li>
+</template> 
+
+<script>
+export default {
+    props: {
+        pokemon:Object
+    }
+};
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+  max-width: 300px;
+}
+</style>
---
+++ src/components/Pokemons.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+      <Pokemon v-for="pokemon in pokemons"
+        v-bind:key="pokemon.name"
+        v-bind:pokemon="pokemon"/>
+  </ul>      
+</template>
+
+<script>
+      
+import pokemonsApi from '../services/pokedex.js';
+import Pokemon from './Pokemon.vue';
+
+export default {
+    data() {
+        return {
+            pokemons: pokemonsApi
+        };
+    },
+    components: {
+        Pokemon
+    }
+};
+</script>
+      
+      <style scoped>
+      
+      </style>
+      
