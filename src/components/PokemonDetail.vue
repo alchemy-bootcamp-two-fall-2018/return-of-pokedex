92eacabc2914fea15c19a6038fcf01a5b993ca13
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-11-27 15:16:10 -0800

    shows pics of pokemons

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -1,7 +1,12 @@
 <template>
     <li>
         <div class="info">
+            <img v-bind:src="pokemon.url_image"/>
             <h3>{{pokemon.pokemon}}</h3>
+            <p>Type 1: {{pokemon.type_1}}</p>
+            <p>Type 2: {{pokemon.type_2}}</p>
+            <p>Attack: {{pokemon.attack}}</p>
+            <p>Defense:{{pokemon.defense}}</p>
         </div>
     </li>
 </template>
