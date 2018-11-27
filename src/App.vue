eab19bc809775e9a08896e914bd0f5e8b6df78a7
Marcy Silverman <marcysilve225@gmail.com>
2018-11-27 14:49:50 -0800

    Files set up and pokemon pictures are rendering with description

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app"> 
+    <Pokemons v-bind:pokemons="pokemons"/>
+  </div>
+</template>
+
+<script>
+import pokemons from '../pokedex.js'; 
+import Pokemons from './components/Pokemons.vue'; 
+
+export default {
+    data() {
+        return {
+            pokemons,
+        }; 
+    },
+    components: {
+        Pokemons
+    }
+};
+</script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="info">
+    <li>
+            <img v-bind:src="pokemon.url_image"/>
+            <h3> {{ pokemon.pokemon }}</h3>
+            <p> ability: {{ pokemon.ability_1}}</p>
+            <p> attack: {{ pokemon.attack}}</p>
+            <p> weight: {{ pokemon.weight}}</p>
+    </li>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pokemon: Object
+    }
+};
+</script>
+
+<style scoped>
+li {
+  position: relative;
+  /* height: 50px; */
+}
+img {
+  /* position: absolute; */
+  top: 0;
+  left: 0;
+  width: 100px;
+  height: 100px;
+  z-index: -1;
+  object-fit: cover;
+}
+/* .info {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50px; 
+  background: rgba(255, 255, 255, 0.8);
+  text-align: center;
+} */
+h3, p {
+  margin: 0;
+}
+li {
+    border: 1px solid red;
+    margin: 10px;
+}
+</style>
