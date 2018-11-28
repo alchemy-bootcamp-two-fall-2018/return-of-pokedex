cccf61694b4368dc7e83c2075bcdc909e60909a6
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-11-28 03:35:13 -0700

    type 2 filtering

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -3,22 +3,24 @@
         <h3>{{pokemon.pokemon}}</h3>
         <img v-bind:src="pokemon.url_image">
         <div id="stats">
-            <h4>Type: {{pokemon.type_1}}</h4>
+            <h4>Type 1: {{pokemon.type_1}}</h4>
+            <h4>Type 2: {{pokemon.type_2}}</h4>
             <h4>ATK: {{pokemon.attack}}</h4>
             <h4>DEF: {{pokemon.defense}}</h4>
         </div>   
     </li>
 </template>
- <script>
+
+<script>
 export default {
     props: {
         pokemon: Object
     }
 }
 </script>
- <style>
+
+<style>
     img {
         width: 50px;
     }
-    
- </style>
+</style>
