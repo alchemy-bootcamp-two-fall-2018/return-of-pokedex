1990af9cc21630554728570faea8c4a397d3e02a
JeiWolfei <jvampy22@gmail.com>
2018-11-27 21:49:44 -0800

    fixed pokemons.vue

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,45 @@
+<template>
+    <li>
+        <img v-bind:src="pokemon.url_image" />
+        <div class="info" v-bind:style="{ color: pokemon.color_1 }">
+            <h3>{{ pokemon.pokemon }}</h3>
+            <p>Type 1: {{pokemon.type_1}}</p>
+            <p>Type 2: {{pokemon.type_2}}</p> 
+            <p>Attack: {{pokemon.attack}}</p>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        pokemon: Object
+    }
+};
+</script>
+
+<style>
+    li {
+        position: relative;
+        height: 300px;
+        border: 2px solid rgb(243, 18, 18);
+    }
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        object-fit: cover;
+    }
+    .info {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 100px;
+        background: rgba(255, 255, 255, 0.8);
+        line-height: 1em;
+    }
+    
+</style>
