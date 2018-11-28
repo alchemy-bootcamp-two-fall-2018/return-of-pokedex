2fd2847e1a31daf5063dc5596f9a40cf34a865e2
Heather <unphurlng@gmail.com>
2018-11-27 17:33:07 -0800

    connected App and api, printing all pokedex data to console.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{pokethem}}
+    <!-- <Pokethem /> -->
   </div>
-</template>
+</template> 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import Pokethem from './components/Pokethem';
+import pokethemApi from './services/pokethemApi.js';
+
 
 export default {
+    data() {
+        return {
+            pokethem: pokethemApi.getPokethem(),
+        };
+    },
     name: 'app',
     components: {
-        HelloWorld
+        // Pokethem
     }
 };
 </script>
