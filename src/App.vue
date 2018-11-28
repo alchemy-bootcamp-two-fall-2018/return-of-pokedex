36355cf1279376e1b5cf0265672e2b5b1188b483
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-27 16:30:46 -0800

    filters types

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-
-    
+    <Header 
+      v-bind:types="pokemonTypes"
+      v-bind:filter="filter"
+    />
     <Pokedex v-bind:pokemons="pokemons" />
   </div>
 </template>
@@ -9,18 +11,45 @@
 <script>
 import pokemonApi from './pokemonsApi.js';
 import Pokedex from './components/Pokedex';
+import Header from './components/Header';
 
 export default {
     data() {
         return {
             pokemons: pokemonApi.getPokemons(), 
+
+            filter: {
+                type_1: '',
+                attack: 0,
+                defense: 0
+            },
         };
     },
 
     components: {
-        Pokedex
+        Pokedex,
+        Header
     },
     
+    computed: {
+        pokemonTypes() {
+            const types = [];
+            this.pokemons.forEach(pokemon => {
+                if(!types.includes(pokemon.type_1)) {
+                    types.push(pokemon.type_1);
+                }
+            });
+            return types;
+        },
+        // filtered(){ 
+        //     return this.pokemons.filter(pokemon => {
+                
+                
+        //     });
+            
+        // }
+     
+    }
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <label>
+            Type:
+            <select v-model="filter.type_1">
+                <option value="">All</option>
+                <option
+                    v-for="type in types"
+                    v-bind:key="type"
+                    v-bind:value="type">
+                    {{type}}
+                </option>
+            </select>
+        </label>
+
+
+        
+    </header>
+
+</template>
+
+<script>
+export default {
+    props: {
+        types: Array,
+        filter: Object
+        
+    }
+};
+</script>
+
+<style scoped>
+header {
+  background: steelblue;
+  color: white;
+  padding: 10px;
+}
+</style>
