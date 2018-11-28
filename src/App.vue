6f46dcfe5841cdd41f734e2d9068b08289ca84db
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-11-28 03:25:22 -0700

    all pokemon are displaying

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Pokedex v-bind:pokemons="pokemons"/>
+  </div>
+</template>
+ <script>
+import pokemonApi from './pokemonApi.js';
+import Pokedex from './components/Pokedex.vue';
+export default {
+  data() {
+    return {
+      pokemons: pokemonApi.getAll(),
+    };
+  },
+  components: {
+    Pokedex
+  },
+}
+</script>
+ <style>
+ </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Sort goes here</h2>
+        <h2>Filter goes here</h2>
+    </div>
+</template>
+ <script>
+export default {
+ }
+</script>
+ <style>
+ </style>
