1c55670c35cf591c851ec96c6166e60dfe956876
Heather <unphurlng@gmail.com>
2018-12-04 07:23:19 -0800

    build out of header labels

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,22 +2,55 @@
     <header>
         <label>
             <strong>Name: </strong>
-            <input type="text">
+            <select v-model="filter.pokemon">
+                <option value="">Any</option>
+                <option
+                    v-for="pokemon in types"
+                    :key="pokemon"
+                    :value="pokemon">
+                    {{pokemon}}
+                </option>
+            </select>
+
         </label>
 
         <label>
             <strong>Type 1: </strong>
-            <input type="text">
+            <select v-model="filter.type_1">
+                <option value="">Any</option>
+                <option
+                    v-for="type1 in types"
+                    :key="type1"
+                    :value="type1">
+                    {{type1}}
+                </option> 
+            </select>
+
         </label>
 
         <label>
             <strong>Attack: </strong>
-            <input v-model.number="filter.attack" type="number">
+            <input type="number" v-model="filter.attack">
         </label>
 
         <label>
             <strong>Defense: </strong>
-            <input type="number">
+            <input type="number" v-model="filter.defense">
+        </label>
+
+        <label>
+            Sort By:
+            <select v-model="sort.field">
+                <option value="name">Pokemon Name</option>
+                <option value="type">Type 1</option>
+                <option value="attack">Attack Points</option>
+                <option value="defense">Defense Points</option>
+            </select>
+
+            <select v-model="sort.direction">
+                <option value="1">Asc</option>
+                <option value="-1">Dsc</option>
+            </select>
         </label>
     </header>
 </template>
@@ -26,6 +59,8 @@
 export default {
     props: {
         filter: Object
+        // // types: Array,
+        // sort: Object
     }
 };
 </script>
