721e925f7121e249d63911d2cd9726d41d226f4d
‘Michael <pickup.patterns@gmail.com>
2018-12-05 08:14:46 -0800

    better form styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,53 +3,55 @@
         <h1>POKEDEX</h1>
         <img scr="http://pngimg.com/uploads/pokemon_logo/pokemon_logo_PNG14.png">
         <!--<img src="http://www.pokestadium.com/sprites/black-white/animated/shiny/pichu.gif">-->
-        <label>
-            NAME:
-            <input type="text"
-            v-model="filter.pokemon" >
+        <div class="form">
+          <label>
+              NAME:
+              <input type="text"
+              v-model="filter.pokemon" >
+          </label>
+          <label>
+          TYPE ONE:
+          <select v-model="filter.type_1">
+            <option value="">ANY</option>
+            <option v-for="type in types" v-bind:key="type" v-bind:value="type">
+              {{type}}
+            </option>
+          </select>
         </label>
         <label>
-        TYPE ONE:
-        <select v-model="filter.type_1">
-          <option value="">ANY</option>
-          <option v-for="type in types" v-bind:key="type" v-bind:value="type">
-            {{type}}
-          </option>
-        </select>
-      </label>
-       <label>
-        TYPE TWO:
-        <select v-model="filter.type_2">
-          <option value="">ANY</option>
-          <option v-for="type in types" v-bind:key="type" v-bind:value="type">
-            {{type}}
-          </option>
-        </select>
-      </label>
-       <label>
-            ATTACK
-            <input type="number"
-                v-model.number="filter.attack" min="30" step="5">
+          TYPE TWO:
+          <select v-model="filter.type_2">
+            <option value="">ANY</option>
+            <option v-for="type in types" v-bind:key="type" v-bind:value="type">
+              {{type}}
+            </option>
+          </select>
         </label>
         <label>
-            DEFENSE
-            <input type="number"
-                v-model.number="filter.defense" min="30" step="5">
-        </label>
-         SORT BY:
-         <label>  
-      <select v-model="sort.field">
-        <option value="pokemon">NAME</option>
-        <option value="type_1">TYPE 02</option>
-        <option value="attack">ATTACK</option>
-        <option value="defense">DEFENSE</option>
+              ATTACK
+              <input type="number"
+                  v-model.number="filter.attack" min="30" step="5">
+          </label>
+          <label>
+              DEFENSE
+              <input type="number"
+                  v-model.number="filter.defense" min="30" step="5">
+          </label>
+          SORT BY:
+          <label>  
+        <select v-model="sort.field">
+          <option value="pokemon">NAME</option>
+          <option value="type_1">TYPE 02</option>
+          <option value="attack">ATTACK</option>
+          <option value="defense">DEFENSE</option>
 
-      </select>
-      <select v-model="sort.direction">
-        <option value="1">Asc</option>
-        <option value="-1">Desc</option>
-      </select>
-    </label>
+        </select>
+        <select v-model="sort.direction">
+          <option value="1">Asc</option>
+          <option value="-1">Desc</option>
+        </select>
+      </label>
+    </div>
     </header>
 </template>
 
@@ -71,5 +73,10 @@ background: black;
 color: white;
 padding: 10px;
 height:400px;
+} 
+label {
+  display:block;
+  text-align: left;
+  
 }
 </style>
