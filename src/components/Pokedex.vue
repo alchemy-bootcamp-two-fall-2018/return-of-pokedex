622370d15c5d2747649f3e96ddf45d762f4c53eb
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-12 14:54:59 -0800

    staggering list transition works!

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,9 +1,17 @@
 <template>
-    <ul>
+    <transition-group n
+    ame="flip-list" 
+    tag="ul"
+    v-bind:css="false"
+    v-on:before-enter="beforeEnter"
+    v-on:enter="enter"
+    v-on:leave="leave">
+   
         <Pokemon v-for="pokemon in pokemons"
+        @click.native="onSelect(pokemon)"
         v-bind:key="pokemon.pokemon"
         v-bind:pokemon="pokemon"/>
-    </ul>
+    </transition-group>
 </template>
 
 <script>
@@ -12,16 +20,47 @@ import Pokemon from './Pokemon.vue';
 
 export default {
     props: {
-        pokemons: Array
+        pokemons: Array,
+        onSelect: Function
     },
     
     components: {
         Pokemon
+    },
+
+    methods: {
+        beforeEnter: function(el) {
+            el.style.opacity = 0;
+            el.style.height = 0;
+        },
+        enter: function(el, done) {
+            var delay = el.dataset.index * 150;
+            setTimeout(function() {
+                /* eslint-disable-next-line */
+                Velocity(
+                    el,
+                    { opacity: 1, height: '250px' },
+                    { complete: done }
+                );
+            }, delay);
+        }, 
+        leave: function(el, done) {
+            var delay = el.dataset.index * 150;
+            setTimeout(function() {
+                /* eslint-disable-next-line */
+                Velocity(
+                    el,
+                    { opacity: 0, height: 0 },
+                    { complete: done }
+                );
+            }, delay);
+        },
     }
+
 };
 </script>
 
-<style scoped>
+<style scoped lang="postcss">
     ul {
             display: grid;
             grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
@@ -30,5 +69,8 @@ export default {
             list-style-type: none;
             margin: 0;
             padding: 0;
+            height: calc(100vh - 150px);
+            width: 50vw;
+            overflow-y: auto;
         };
 </style>
