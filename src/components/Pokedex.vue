55548e4c85508f2263ff14ff34a9afa88b57174b
Ivan <i.poblete915@gmail.com>
2018-12-12 15:36:49 -0800

    console logged select pokemon

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,28 +1,47 @@
 <template>
-  <ul>
+  <transition-group name="staggered-fade" tag="ul">
     <Pokemon v-for="pokemon in pokemons"
       v-bind:key="pokemon.pokemon"
-      v-bind:pokemon="pokemon"/>
-  </ul>
+      v-bind:pokemon="pokemon"
+      v-bind:selected="selected"
+      v-bind:onSelect="handleSelect"/>
+  </transition-group>
 </template>
 
 <script>
 import Pokemon from './Pokemon.vue';
+// import PokemonDetail from './PokemonDetail.vue';
 
 export default {
+  data() {
+    return {
+      selected: null
+    };
+  },
   props: {
     pokemons: Array
   },
   components: {
     Pokemon
+  },
+  methods: {
+    handleSelect(pokemon) {
+      console.log('hello', pokemon.pokemon);
+      this.selected = pokemon;
+    }
   }
 };
 </script>
 
-<style>
+<style lang="postcss">
+
 ul {
   display: grid;
   grid-template-columns: repeat(4, minmax(200px, 1fr));
   list-style-type: none;
+}
+
+.staggered-fade {
+  transition: transform 1s;
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,20 +1,21 @@
 <template>
-  <li class="info">
-    <img v-bind:src="pokemon.url_image" />
-    <div>
-      <h3>{{ pokemon.pokemon }}</h3>
-      <p>Type 1: {{ pokemon.type_1 }}</p>
-      <p>Type 2: {{ pokemon.type_2 }}</p>
-      <p>Attack: {{pokemon.attack}}</p>
-      <p>Defense: {{pokemon.defense}}</p>
-    </div>
-  </li>
+  <div>
+    <li v-on:click="onSelect(pokemon)">
+        <img v-bind:src="pokemon.url_image">
+        <h3>{{ pokemon.pokemon }}</h3>
+        <p>Type 1: {{ pokemon.type_1 }}</p>
+        <p>Type 2: {{ pokemon.type_2 }}</p>
+        <p>Attack: {{pokemon.attack}}</p>
+        <p>Defense: {{pokemon.defense}}</p>
+    </li>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
-    pokemon: Object
+    pokemon: Object,
+    onSelect: Function
   }
 };
 </script>
