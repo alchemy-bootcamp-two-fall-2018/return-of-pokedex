e1c035724182885010775e273ea6dc86d4ad5471
Cheri Portman <cheriportman@hotmail.com>
2018-12-12 14:56:36 -0800

    staggering transition and bounce

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -25,7 +25,8 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, .5);
+  background-image:url(https://static.tumblr.com/bfd42c6e12c4a48a0630ca3b0fcd1d15/vt0qhql/KSbnz4jg0/tumblr_static_tumblr_static_filename_640.jpg);
+  /* background-color: rgba(0, 0, 0, .5); */
   z-index: 5;
 }
 .content {
--- src/components/pokemons/Pokemons.vue
+++ src/components/pokemons/Pokemons.vue
@@ -3,15 +3,26 @@
 
 <template>
     <div>
+        <transition name="bounce">
         <Modal v-if="showModal" :onClose="() => showModal = false">
             <PokemonDetail :pokemon="selected"/>
         </Modal>
-        <ul>
+        </transition>
+        <transition-group
+            name="staggered-fade"
+            tag="ul"
+            v-bind:css="false"
+            v-on:before-enter="beforeEnter"
+            v-on:enter="enter"
+            v-on:leave="leave"
+        >
             <Pokemon v-for="pokemon in pokemons"
                 v-bind:key="pokemon.pokemon"
                 v-bind:pokemon="pokemon"
                 :onSelect="handleSelect"/>
-        </ul>
+        
+        </transition-group>
+        
     </div>
 </template>
 
@@ -38,10 +49,35 @@ export default {
     methods: {
         handleSelect(pokemon) {
             this.selected = pokemon;
-            this.showModal = true;
+            this.showModal = true;   
+        },
+        beforeEnter: function(el) {
+            el.style.opacity = 0;
+            el.style.height = 0;
+        },
+        enter: function(el, done) {
+            var delay = el.dataset.index * 150;
+            setTimeout(function() {
+                /*eslint-disable-next-line*/
+                Velocity(
+                    el,
+                    { opacity: 1, height: '300px' },
+                    { complete: done }
+                );
+            }, delay);
+        },
+        leave: function(el, done) {
+            var delay = el.dataset.index * 150;
+            setTimeout(function() {
+                /*eslint-disable-next-line*/
+                Velocity(
+                    el,
+                    { opacity: 0, height: 0 },
+                    { complete: done }
+                );
+            }, delay);
         }
     }
-
 };
 </script>
 
@@ -53,4 +89,21 @@ export default {
         margin: 0;
         padding: 0;
     }
+    .bounce-enter-active {
+        animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+    }
+    @keyframes bounce-in {
+    0% {
+        transform: scale(0);
+    }
+    50% {
+        transform: scale(1.5);
+    }
+    100% {
+        transform: scale(1);
+    }
+}
 </style>
