fa74677e55274fe659f08ea1e9915c87c3f85f10
Ivan <i.poblete915@gmail.com>
2018-12-12 17:11:05 -0800

    wired up close button on Modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <p>Type 1: {{ pokemon.type_1 }}</p>
-    <p>Type 2: {{ pokemon.type_2 }}</p>
-    <p>Attack: {{pokemon.attack}}</p>
-    <p>Defense: {{pokemon.defense}}</p>
+  <div class="modal" @click="onClose">
+    <div class="content" @click.stop="">
+      <button class="close" @click="onClose">X</button>
+      <p>Type 1: {{ pokemon.type_1 }}</p>
+      <p>Type 2: {{ pokemon.type_2 }}</p>
+      <p>Attack: {{pokemon.attack}}</p>
+      <p>Defense: {{pokemon.defense}}</p>
+    </div>
   </div>
 </template>
 
@@ -11,11 +14,31 @@
 
 export default {
   props: {
-    pokemon: Object
+    pokemon: Object,
+    onClose: Function
   }
 };
 </script>
 
 <style>
-
+.modal {
+  position: fixed;
+  top: 0; left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, .5);
+}
+.content {
+  position: relative;
+  background: white;
+  padding: 40px;
+}
+.close {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -3,10 +3,6 @@
     <li v-on:click="onSelect(pokemon)">
         <img v-bind:src="pokemon.url_image">
         <h3>{{ pokemon.pokemon }}</h3>
-        <!-- <p>Type 1: {{ pokemon.type_1 }}</p>
-        <p>Type 2: {{ pokemon.type_2 }}</p>
-        <p>Attack: {{pokemon.attack}}</p>
-        <p>Defense: {{pokemon.defense}}</p> -->
         <button @click="showModal = true">Details</button>
     </li>
 
